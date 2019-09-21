66e058eb84b088cb7cac59ca26f7524a2becaa2d
Laiz Lira <laizgamalira@gmail.com>
2019-09-21 19:42:55 -0300

    Remove /build do gitignore

---
+++ build/precache-manifest.5b1e6a2158bf4f709c37e834ab0f64c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab687f9f41bef896a420e73c15fced13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45eff487db97703913ae",
+    "url": "/static/css/main.a6017fa1.chunk.css"
+  },
+  {
+    "revision": "61c8c26cbecc4d327b28",
+    "url": "/static/js/2.8d6ee4ec.chunk.js"
+  },
+  {
+    "revision": "45eff487db97703913ae",
+    "url": "/static/js/main.ad5058f0.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "8ed8416a56a6a9078b0f22f9a1b2a934",
+    "url": "/static/media/myself.8ed8416a.jpg"
+  }
+]);
